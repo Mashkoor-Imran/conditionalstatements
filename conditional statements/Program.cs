6a0f78d3e94677de4dc79d1c2d69368538ba1813
Mashkoor <mashkoorimran3@gmail.com>
2024-10-10 23:46:07 -0700

    Conditional statements proj

---
+++ conditional statements/Program.cs
@@ -0,0 +1,20 @@
+﻿public class ConditionalStatements
+{
+    public static void Main(string[] args)
+    {
+       Console.WriteLine("Please Enter Your Marks");
+        int m=Convert.ToInt32(Console.ReadLine());
+        if (m >= 33)
+            {
+            Console.WriteLine("Your are Pass");
+            Console.ReadLine();
+
+        }
+        else
+        {
+            Console.WriteLine("Your are fail");
+            Console.ReadLine();
+        }
+
+    }
+}
